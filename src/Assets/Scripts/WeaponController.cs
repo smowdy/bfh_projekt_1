1bac83420c723c6fbd7c7620d63f9ca7d01b41d2
Kevin Riesen <kevin.riesen@hotmail.com>
2019-10-21 19:28:15 +0200

    feat: Added Weapon and its rotation to cursor

---
+++ src/Assets/Scripts/WeaponController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Aim();
+    }
+
+    private void Aim()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(transform.up, transform.position);
+        float distance;
+        if (plane.Raycast(ray, out distance))
+        {
+            Vector3 targetPosition = ray.GetPoint(distance);
+            Vector3 direction = targetPosition - transform.position;
+            float rotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.eulerAngles = new Vector3(0, rotation, 0);
+        }
+    }
+}
