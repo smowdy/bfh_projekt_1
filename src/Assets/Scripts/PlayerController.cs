d764a5be1a0f7bac11d600525d339afb849f3f51
mullm <48676445+mullm@users.noreply.github.com>
2019-10-23 18:10:21 +0200

    Update PlayerController.cs

--- src/Assets/Scripts/PlayerController.cs
+++ src/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Transform myTransform;
 
+    [SerializeField]float movementSpeed = 32f;
+    [SerializeField]float turnSpeed = 40f;
+
+    private void Start()
+    {
+        myTransform = transform;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        Turn();
+        Thrust();
     }
+
+    void Turn()
+    {
+        float yaw = turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+
+        myTransform.Rotate(0, yaw, 0);
+    }
+    void Thrust() {
+        myTransform.position += transform.forward * movementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+
+    }
+
 }
