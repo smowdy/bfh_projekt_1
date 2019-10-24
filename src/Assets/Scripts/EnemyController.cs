d6db63347faaf3fc38bfd1e5b77755541039b126
ueli <buehler.ueli94@gmail.com>
2019-10-24 21:17:08 +0200

    unecessary line removed

--- src/Assets/Scripts/EnemyController.cs
+++ src/Assets/Scripts/EnemyController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField] float movementSpeed = 2f;
-    [SerializeField] float turnSpeed = 2f;
+    float movementSpeed = 5f;
+    float turnSpeed = 10f;
     float turnDirection = 1;
 
     private void Start()
@@ -22,13 +22,11 @@ public class EnemyController : MonoBehaviour
     void Turn()
     {
         float yaw = turnSpeed * Time.deltaTime * turnDirection;
-
         transform.Rotate(0, yaw, 0);
     }
     void Thrust()
     {
         transform.position += transform.forward * movementSpeed * Time.deltaTime;
-
     }
 
     private void SetRandomDirection()
