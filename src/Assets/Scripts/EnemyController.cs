76b251c5a9ca5377fb3c6f0714de82f75220a5ed
ueli <buehler.ueli94@gmail.com>
2019-10-24 20:21:54 +0200

    enemyship controller created

--- src/Assets/Scripts/EnemyController.cs
+++ src/Assets/Scripts/EnemyController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float movementSpeed = 2f;
+    [SerializeField] float turnSpeed = 2f;
+    float turnDirection = 1;
 
+    private void Start()
+    {
+        InvokeRepeating("SetRandomDirection", 0.1f, 3.0f);
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        Turn();
+        Thrust();
+    }
+
+    void Turn()
+    {
+        float yaw = turnSpeed * Time.deltaTime * turnDirection;
+
+        transform.Rotate(0, yaw, 0);
+    }
+    void Thrust()
+    {
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+
+    }
+
+    private void SetRandomDirection()
+    {
+        if (Random.Range(-1, 1) >= 0)
+        {
+            turnDirection = 1;
+        }
+        else
+        {
+            turnDirection = -1;
+        }
     }
 }
