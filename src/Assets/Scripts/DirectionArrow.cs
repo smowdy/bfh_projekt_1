9e545530b79ba32f424c55cdefbf49193ef9de30
mullm <48676445+mullm@users.noreply.github.com>
2019-11-07 18:31:55 +0100

    Add arrow and distance to target

---
+++ src/Assets/Scripts/DirectionArrow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionArrow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = target.transform.position;
+        targetPosition.y = transform.position.y;
+        transform.LookAt(targetPosition);
+        Debug.Log(targetPosition);
+    }
+}
