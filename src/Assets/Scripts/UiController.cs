6d55cb2805649e16cca4b31f7570437b2536efe7
ueli <buehler.ueli94@gmail.com>
2019-11-07 15:03:15 +0100

    player healthbar implemented

--- src/Assets/Scripts/UiController.cs
+++ src/Assets/Scripts/UiController.cs
@@ -8,14 +8,26 @@ public class UiController : MonoBehaviour
     [SerializeField]
     private GameObject Crosshair;
 
+    [SerializeField]
+    private GameObject Bar;
+
+    [SerializeField]
+    private PlayerController PlayerController;
+
     // Update is called once per frame
     public void Update()
     {
         SetCrosshairPosition();
+        SetHealthbarSize(PlayerController.GetCurrentHealthpointsNormalized());
     }
 
     private void SetCrosshairPosition()
     {
         Crosshair.transform.position = Input.mousePosition;
+    }
+
+    public void SetHealthbarSize(float sizeNormalized)
+    {
+        Bar.transform.localScale = new Vector3(sizeNormalized, 1f);
     }
 }
