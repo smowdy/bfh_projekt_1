af921e890ba150140bb6c4f8929152f1850d8dff
Kevin Riesen <kevin.riesen@hotmail.com>
2019-11-13 21:25:07 +0100

    feat: boundry that damages outside objects

---
+++ src/Assets/Scripts/BoundryBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BoundryBehaviour : MonoBehaviour
+{
+    private List<DestructibleObjectController> objectsOutsideBoundry = new List<DestructibleObjectController>();
+
+    private void FixedUpdate()
+    {
+        DamageObjectsOutsideBoundry();
+    }
+
+    private void DamageObjectsOutsideBoundry()
+    {
+        foreach (var destructible in objectsOutsideBoundry)
+        {
+            destructible.TakesDamage(1);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var destructible = other.gameObject.GetComponent<DestructibleObjectController>();
+        if (destructible == null) { return; }
+        objectsOutsideBoundry.Remove(destructible);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var destructible = other.gameObject.GetComponent<DestructibleObjectController>();
+        if(destructible == null)
+        {
+            Destroy(other.gameObject);
+            return;
+            
+        }
+
+        if (objectsOutsideBoundry.Any(x => x.Equals(destructible))) { return; }
+        objectsOutsideBoundry.Add(destructible);
+    }
+}
