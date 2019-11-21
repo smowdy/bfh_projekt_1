2301863cd14a217a68c83e2c1f32f0049f2ef568
ueli <buehler.ueli94@gmail.com>
2019-11-21 17:38:39 +0100

    fix healbar

---
+++ src/Assets/Scripts/PositionController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionController : MonoBehaviour
+
+    
+{
+    [SerializeField]
+    RectTransform parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RectTransform rectTransform = transform.GetComponent<RectTransform>();
+        transform.localPosition = new Vector3(-parent.rect.width/2 + rectTransform.rect.width/2 + 50, -parent.rect.height/2 + rectTransform.rect.height/2 + 50, 0);
+    }
+}
