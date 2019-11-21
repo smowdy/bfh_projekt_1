4c590d78e762980c58a413650ba3911e04a3cbe9
ueli <buehler.ueli94@gmail.com>
2019-11-21 14:38:59 +0100

    rotate and fire if in range

--- src/Assets/Scripts/EnemyWeaponController.cs
+++ src/Assets/Scripts/EnemyWeaponController.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 public class EnemyWeaponController : WeaponController
 {
     // Update is called once per frame
+
+    [SerializeField]
+    private Transform target;
+
     void Update()
     {
+        if(Vector3.Distance(transform.position, target.position) <= 10)
+        {
+Aim((target.position - transform.position).normalized);
         TryShoot("enemy");
+        }
+        
     }
 }
