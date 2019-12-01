fd64ab4a5a2e2f2ba1b1c6b55743fcb4a409ceb0
Kevin Riesen <kevin.riesen@hotmail.com>
2019-12-01 14:22:58 +0100

    feat: particle system for destruction

---
+++ src/Assets/Scripts/DestructionAnimator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructionAnimator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject destructionParticleSystem;
+
+    public void Play()
+    {
+        Instantiate(destructionParticleSystem, transform.position, transform.rotation);
+    }
+}
