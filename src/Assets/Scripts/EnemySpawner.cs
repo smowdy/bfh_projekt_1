f35e6c94e3fdf22f16f0d7810abe78b073f23cc2
ueli <buehler.ueli94@gmail.com>
2019-12-05 14:30:50 +0100

    spawn in save distance

--- src/Assets/Scripts/EnemySpawner.cs
+++ src/Assets/Scripts/EnemySpawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class EnemySpawner : MonoBehaviour
 {
@@ -9,13 +10,37 @@ public class EnemySpawner : MonoBehaviour
 
     int numberOfEnemies = 20;
 
+    int safetyDistance = 30;
 
-    // Start is called before the first frame update
+
     void Awake()
     {
-        for (int i=0; i < numberOfEnemies; i++)
+        SpawnEnemies();
+    }
+
+    private void SpawnEnemies()
+    {
+        int i = 0;
+        while (i < numberOfEnemies)
         {
-            Instantiate(enemyPrefab, new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100)), Quaternion.identity);
+            Vector3 spawnPos = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100));
+            if (IsValidSpawnPos(spawnPos))
+            {
+                Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+                i++;
+            }
+            
         }
+    }
+
+    private bool IsValidSpawnPos(Vector3 spawnPos)
+    {
+        return IsNotInReachOf(spawnPos, "player") || IsNotInReachOf(spawnPos, "goal");
+    }
+
+    private bool IsNotInReachOf(Vector3 spawnPos, string tag)
+    {
+        GameObject target = GameObject.FindGameObjectsWithTag(tag).FirstOrDefault();
+        return Vector3.Distance(target.transform.position, spawnPos) >= safetyDistance;
     }
 }
