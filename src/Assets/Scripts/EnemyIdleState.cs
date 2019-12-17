0c8d42233f068448856c2af2a56edb81e368de70
ueli <buehler.ueli94@gmail.com>
2019-12-17 19:37:33 +0100

    optimising

--- src/Assets/Scripts/EnemyIdleState.cs
+++ src/Assets/Scripts/EnemyIdleState.cs
@@ -7,7 +7,7 @@ public class EnemyIdleState : EnemyState
     private float turnDirection = 1;
     private float nextDirectionUpdateAt = 0;
     private float detectionDistance = 10f;
-    private float rayCastOffset = 3.5f;
+    private float rayCastWidth = 3.5f;
 
 
     public EnemyIdleState(GameObject enemy, float engageDistance) : base(enemy, engageDistance) {
@@ -20,41 +20,36 @@ public class EnemyIdleState : EnemyState
         {
             return new EnemyEngageState(enemy, engageDistance);
         }
+
         RaycastHit hit;
         Vector3 raycastOffset = Vector3.zero;
-        Vector3 left = enemy.transform.position - enemy.transform.right * rayCastOffset;
-        Vector3 right = enemy.transform.position + enemy.transform.right * rayCastOffset;
+        Vector3 leftDetectionRay = enemy.transform.position - enemy.transform.right * rayCastWidth;
+        Vector3 rightDetectionRay = enemy.transform.position + enemy.transform.right * rayCastWidth;
 
-        Debug.DrawRay(left, enemy.transform.forward * detectionDistance, Color.cyan);
-        Debug.DrawRay(right, enemy.transform.forward * detectionDistance, Color.cyan);
+        Debug.DrawRay(leftDetectionRay, enemy.transform.forward * detectionDistance, Color.cyan);
+        Debug.DrawRay(rightDetectionRay, enemy.transform.forward * detectionDistance, Color.cyan);
 
-        if(Physics.Raycast(left, enemy.transform.forward, out hit, detectionDistance))
+        if(Physics.Raycast(leftDetectionRay, enemy.transform.forward, out hit, detectionDistance))
         {
             raycastOffset += Vector3.right;
-        }else if(Physics.Raycast(right, enemy.transform.forward, out hit, detectionDistance))
+        }else if(Physics.Raycast(rightDetectionRay, enemy.transform.forward, out hit, detectionDistance))
         {
             raycastOffset -= Vector3.right;
         }
 
-        if (Time.time >= nextDirectionUpdateAt)
-        {
-            SetRandomDirection();
-        }
-
         if(raycastOffset == Vector3.left)
         {
-            //enemy.transform.Rotate(raycastOffset * 50f * Time.deltaTime);
-            enemy.GetComponent<EnemyController>().Turn(-1);
+            turnDirection = -1;
         }
         else if (raycastOffset == Vector3.right) 
         {
-            enemy.GetComponent<EnemyController>().Turn(1);
-        }
-        else
+            turnDirection = 1;
+        }else if (Time.time >= nextDirectionUpdateAt)
         {
-            enemy.GetComponent<EnemyController>().Turn(turnDirection);
+            SetRandomDirection();
         }
-
+        
+        enemy.GetComponent<EnemyController>().Turn(turnDirection);
         enemy.GetComponent<EnemyController>().Thrust(1);
         return this;
     }
